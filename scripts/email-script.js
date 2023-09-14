e7a0224acd3f7579c2b7aaf280933ce10f7643aa
darko <darkonikoloc@gmail.com>
2023-09-14 09:41:55 +0200

    finished for now, email service done

--- scripts/email-script.js
+++ scripts/email-script.js
@@ -1,3 +1,53 @@
 function sendEmail() {
-  var 
+  var params = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value
+  };
+
+  if(params.name === '') {
+    document.getElementById('name').style.backgroundColor = '#fffd8d';
+    setTimeout(() => {
+      document.getElementById('name').style.backgroundColor = '#fffdaf';
+    }, 1000);
+    return;
+  }
+  if(params.email === '') {
+    document.getElementById('email').style.backgroundColor = '#fffd8d';
+    setTimeout(() => {
+      document.getElementById('email').style.backgroundColor = '#fffdaf';
+    }, 1000);
+    return;
+  }
+  if(params.message === '') {
+    document.getElementById('message').style.backgroundColor = '#fffd8d';
+    setTimeout(() => {
+      document.getElementById('message').style.backgroundColor = '#fffdaf';
+    }, 1000);
+    return;
+  }
+
+  document.querySelector('.contact-page button').innerHTML = 'Sending...';
+  const serviceID = 'service_7qz0wjj';
+  const templateID = 'template_hf2747b';
+
+  emailjs.send(serviceID, templateID, params).then((res) => {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+    document.querySelector('.contact-page button').innerHTML = 'Sent!';
+    document.querySelector('.contact-page button').style.backgroundColor = '#ffb6c1';
+    setTimeout(() => {
+      document.querySelector('.contact-page button').innerHTML = 'Send';
+      document.querySelector('.contact-page button').style.backgroundColor = '#fffc46';
+    }, 1500);
+  }).catch((error) => {
+    console.log(error);
+    document.querySelector('.contact-page button').innerHTML = 'Try Again';
+  });
+
 }
+
+document.querySelector('.contact-page button').addEventListener('click', () => {
+  sendEmail();
+});
--- scripts/sidebar-script.js
+++ scripts/sidebar-script.js
@@ -1,4 +1,26 @@
+function scrollTo(elementClass) {
+  document.querySelector(elementClass).scrollIntoView();
+}
+
 document.querySelector('.js-sidebar-button')
   .addEventListener('click', () => {
       document.body.classList.toggle('sidebar-open');
   });
+
+document.querySelector('.js-home').addEventListener('click', () => {
+  scrollTo('body');
+});
+document.querySelector('.js-about-me').addEventListener('click', () => {
+  scrollTo('.about-me-page');
+});
+document.querySelector('.js-my-projects').addEventListener('click', () => {
+  scrollTo('.my-projects-page');
+});
+document.querySelector('.js-work').addEventListener('click', () => {
+  scrollTo('.work-page');
+});
+document.querySelectorAll('.js-contact').forEach((button) => {
+  button.addEventListener('click', () => {
+    scrollTo('.contact-page');
+  });
+});
